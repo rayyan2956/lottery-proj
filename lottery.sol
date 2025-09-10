91dbba142226258c6d9355f19575edd110da4b1e
rayyan2956 <mrayyan5296@gmail.com>
2025-09-10 21:52:38 +0500

    winner function

--- lottery.sol
+++ lottery.sol
@@ -8,12 +8,28 @@ contract Lottery{
     }
 
     receive() external payable { 
-        require(msg.value==1 ether);
+        require(msg.value>=1 ether);
         participants.push(payable(msg.sender));
     }
 
     function getBalance() public view returns (uint){
-        require(msg.sender==manager)
+        require(msg.sender==manager);
         return address(this).balance;
+    }
+    function random() public view returns (uint){
+        return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, participants.length)));
+
+    }
+
+    function selectWinner() public view returns(address){
+        require(msg.sender==manager);
+        require(participants.length>=3);
+
+        uint r = random();
+        uint index = r%participants.length;
+
+        address payable winner;
+        winner=participants[index];
+        return winner;
     }
 }
